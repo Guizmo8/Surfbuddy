f84cb980470becc6e6bb7f67af543a5802a2a7c2
goncalo ferreira <gcunha.ferreira@outlook.com>
2024-08-29 12:36:13 +0100

    Implemented-search-logic

--- app/controllers/surfspots_controller.rb
+++ app/controllers/surfspots_controller.rb
@@ -1,6 +1,9 @@
 class SurfspotsController < ApplicationController
   def index
     @surfspots = Surfspot.all
+    if params[:query].present?
+      @surfspots = Surfspot.super_search(params[:query])
+    end
   end
 
   def show
