9cf349af293416ca798f64f6a9ae6b8498473002
Afonso Melo <afonsomelo27@gmail.com>
2024-09-03 13:01:55 +0100

    alert time setting

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,11 @@ class User < ApplicationRecord
   has_many :alerts, dependent: :destroy
   has_many :surf_spots, through: :favourites, dependent: :destroy
 
+  def wants_alert_now?
+    current_time = Time.zone.now.strftime("%H:%M")
+    start_time = alert_start_time.strftime("%H:%M")
+    end_time = alert_end_time.strftime("%H:%M")
+
+    current_time >= start_time && current_time <= end_time
+  end
 end
